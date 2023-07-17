a0b01214fca5408020bace6c0c97e183de732624
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2023-07-17 13:39:50 +0200

    Fixed syntax for dll export

--- src/Heightmap/HeightmapGenerator.cpp
+++ src/Heightmap/HeightmapGenerator.cpp
@@ -1,6 +1,6 @@
 #include "HeightmapGenerator.h"
 
-THATWORLDS_API float that::height::HeightmapGenerator_GetHeight(HeightmapGenerator* pGenerator, float x, float y)
+extern "C" THATWORLDS_API float that::height::HeightmapGenerator_GetHeight(HeightmapGenerator * pGenerator, float x, float y)
 {
 	return pGenerator->GetHeight(x, y);
 }
