75675aa6a67571c1f28deb719c5a18a417b68694
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-07-19 12:00:19 +0200

    Removed deprecated files
    Removed old Heightmap classes
    Removed river logic

--- src/Heightmap/HeightmapGenerator.cpp
+++
@@ -1,6 +0,0 @@
-#include "HeightmapGenerator.h"
-
-extern "C" THATWORLDS_API float that::height::HeightmapGenerator_GetHeight(HeightmapGenerator * pGenerator, float x, float y)
-{
-	return pGenerator->GetHeight(x, y);
-}
--- src/Rivers/RiversGenerator.cpp
+++
@@ -1,6 +0,0 @@
-#include "RiversGenerator.h"
-
-extern "C" THATWORLDS_API float that::river::RiversGenerator_GetDepth(RiversGenerator * pGenerator, float x, float y)
-{
-    return pGenerator->GetDepth(x,y);
-}
