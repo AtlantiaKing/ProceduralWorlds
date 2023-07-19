756483f6c6f155204ff269047e9c0394918b2cde
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-07-19 12:41:54 +0200

    Removed method from dll
    Removed IsTrue method from the SuccessPredicate from the dll so it can no longer be used by users

--- src/SuccessPredicate.h
+++ src/SuccessPredicate.h
@@ -16,7 +16,7 @@ namespace that
 		THATWORLDS_API SuccessPredicate(std::function<bool(float, float)> predicate);
 		~SuccessPredicate() = default;
 
-		bool THATWORLDS_API IsTrue(float height0, float height1) const;
+		bool IsTrue(float height0, float height1) const;
 
 	private:
 		std::function<bool(float, float)> m_Predicate;
