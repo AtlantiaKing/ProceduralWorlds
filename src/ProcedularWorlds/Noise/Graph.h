1b87d79457dae0883f524a0b1208eeb43c66c333
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2023-08-02 20:14:24 +0200

    Added summary comments

--- src/ProcedularWorlds/Noise/Graph.h
+++ src/ProcedularWorlds/Noise/Graph.h
@@ -16,7 +16,17 @@ namespace that
 		Graph() = default;
 		~Graph() = default;
 
+		/// <summary>
+		/// <para>Add a node to the graph</para> 
+		/// <para>x and y values need to be between [0,1]</para> 
+		/// </summary>
 		THATWORLDS_API void AddNode(float x, float y);
+
+		/// <summary>
+		/// <para>Returns the y-value on the graph [0,1] for a given x value</para> 
+		/// <para>The given x value needs to be between [0,1]</para> 
+		/// <para>A graph needs to have lower (x=0) and upper (x=1) bound nodes to succesfully get a value.</para> 
+		/// </summary>
 		THATWORLDS_API float GetValue(float x) const;
 
 	private:
